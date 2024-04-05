2d973eb9d7e9865061b9c1a8e62dc1da571d8fd1
WarisTheDon <99509063+WarisTheDon@users.noreply.github.com>
2024-04-05 13:23:09 +0200

    laget en mappe for css
    somsagt har dannet en mappe for css filer

---
+++ booksearch/src/Js Elementer/BookList.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+
+const BookList = ({ searchTerm }) => {
+  const [books, setBooks] = useState([]);
+  const [filteredBooks, setFilteredBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        if (!searchTerm) {
+          const response = await fetch(`https://openlibrary.org/search.json?q=james+bond+series`);
+          const data = await response.json();
+          const jamesBondBooks = data.docs.filter(book => book.title.toLowerCase().includes('james bond'));
+          setBooks(jamesBondBooks);
+        } else {
+          const response = await fetch(`https://openlibrary.org/search.json?q=${searchTerm.toLowerCase()}`);
+          const data = await response.json();
+          setFilteredBooks(data.docs.filter(book => book.title)); 
+        }
+      } catch (error) {
+        console.error('Error fetching books:', error);
+        setBooks([]);
+        setFilteredBooks([]);
+      }
+    };
+
+    fetchBooks();
+  }, [searchTerm]);
+
+  const getImageUrl = book => {
+    return `https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : 'default'}-L.jpg`;
+  };
+
+  return (
+    <div>
+      {searchTerm ? (
+        <div>
+          <h2>Search Results</h2>
+          <ul>
+            {filteredBooks.map((book, index) => (
+              <li key={index}>
+                <div>Title: {book.title}</div>
+                <div>First Published: {book.first_publish_year}</div>
+                <div>Author: {book.author_name}</div>
+                <div>Average Rating: {book.average_rating}</div>
+                <img src={getImageUrl(book)} alt={book.title} className="book-image" />
+                <a href={`https://www.amazon.com/s?k=${book.title}`} target="_blank" rel="noopener noreferrer">
+                  Buy on Amazon
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div>
+          <h2>James Bond Series</h2>
+          <ul>
+            {books.map((book, index) => (
+              <li key={index}>
+                <div>Title: {book.title}</div>
+                <div>First Published: {book.first_publish_year}</div>
+                <div>Author: {book.author_name}</div>
+                <div>Average Rating: {book.average_rating}</div>
+                <img src={getImageUrl(book)} alt={book.title} className="book-image" />
+                <a href={`https://www.amazon.com/s?k=${book.title}`} target="_blank" rel="noopener noreferrer">
+                  Buy on Amazon
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default BookList;
